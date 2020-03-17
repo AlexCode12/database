e826741f977a6bd5ca1794839bcc302faec22200
alexcode12 <alexander.olsson2@elev.ga.ntig.se>
2020-03-17 11:09:24 +0100

    Initial commit

---
+++ src/DatabaseLoginData.java
@@ -0,0 +1,7 @@
+public class DatabaseLoginData {
+    public static final String DBURL = "localhost";
+    public static final String DBname = "soloadventure";
+    public static String port = "3306";
+    public static String user = "alexander1";
+    public static String password = "password";
+}
