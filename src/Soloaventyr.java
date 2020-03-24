4a9e61ca3f0d3aa9e60d449ec1bd087a2d199540
alexcode12 <alexander.olsson2@elev.ga.ntig.se>
2020-03-24 09:34:04 +0100

    Förbättringar av koden. Undersökt och implementerat andra versioner.

--- src/Soloaventyr.java
+++ src/Soloaventyr.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.Scanner;
 
 public class Soloaventyr {
     public static void main(String[] args) {
@@ -11,29 +12,41 @@ public class Soloaventyr {
 
             // Setup statement
             Statement stmt = conn.createStatement();
-            int filteredID;
-            filteredID = 27;
+            int story_id = 1;
+            int target_id = 1;
             // Create query and execute
-            String strSelect = "select * from story where id = " + filteredID;
-
-            System.out.println("The SQL statement is: " + strSelect + "\n");
-
+            String strSelect = "select body from story where id = " + story_id;
+            System.out.println("Skriv in ID:t för att gå vidare (t.ex skriv 3 om du vill gå till 3");
             ResultSet rset = stmt.executeQuery(strSelect);
 
             // Loop through the result set and print
-
-            System.out.println("The records selected are:");
-            int rowCount = 0;
             while(rset.next()) {
                 String body = rset.getString("body");
-                int    id   = rset.getInt("id");
-                System.out.println(id + "\t" + body);
-                ++rowCount;
+                System.out.println(body);
             }
 
+            while (target_id != 87) {
+                String strSelect2 = "select target_id, description from links where story_id = " + story_id;
+                ResultSet rset2 = stmt.executeQuery(strSelect2);
 
-            System.out.println("Total number of records = " + rowCount);
+                while (rset2.next()) {
+                    int target_ids = rset2.getInt("target_id");
+                    String description = rset2.getString("description");
+                    System.out.println(target_ids + ": " + description);
+                }
 
+                Scanner in = new Scanner(System.in);
+                target_id = in.nextInt();
+                story_id = target_id;
+
+                String strSelect3 = "select body from story where id = " + target_id;
+                ResultSet rset3 = stmt.executeQuery(strSelect3);
+
+                while (rset3.next()) {
+                    String body = rset3.getString("body");
+                    System.out.println(body);
+                }
+            }
             // Close conn and stmt
             conn.close();
             stmt.close();
